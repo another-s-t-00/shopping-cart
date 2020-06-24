ad39c6d1958d57b4f129f82a28604bf9b1c65bae
programming-0001 <67310874+programming-0001@users.noreply.github.com>
2020-06-24 19:06:16 +0900

    Add files via upload

--- staff/staff_list.php
+++ staff/staff_list.php
@@ -21,17 +21,19 @@ $password = '';
 $dbh = new PDO($dsn, $user, $password);
 $dbh->query('SET NAMES utf8');
 
+//<--2.SQL文指令-->
 $sql = 'SELECT code,name FROM mst_staff WHERE 1';
 //「スタッフの名前を全て取り出せ」
 $stmt = $dbh->prepare($sql);
 $stmt->execute();
 
+//<--3.データベースから切断-->
 $dbh = null;
 
 print 'スタッフ一覧<br/><br/>';
 
 print'<form method="post" action="staff_edit.php">';
-//修正画面へ飛べるように
+//スタッフ修正画面へ飛べるようにするフォームを設置
 while(true)
 {
     $rec = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -42,9 +44,14 @@ while(true)
         //もうデータが無ければ、ループから脱出
     }
     print'<input type="radio" name="staffcode" value="'.$rec['code'].'">';
+    //飛び先にスタッフコードを渡すラジオボタンを設置
+    //飛び先ページでは$staff_code=$_POST['staffcode'];でvalueが受け取れる
     print $rec['name'];
     print '</br>';
 }
+print '<input type="submit" value="修正">';
+//postを実行するボタンを設置
+print '</form>';
 
 }
 catch (Exception $e)
