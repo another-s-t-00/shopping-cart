1a9b7c965be89019560b1f476b1a50817b88d8c5
programming-0001 <67310874+programming-0001@users.noreply.github.com>
2020-08-03 20:13:27 +0900

    Add files via upload

--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -78,12 +78,39 @@ session_regenerate_id(true);
             $suryo = $quantity[$i];
             $shokei = $price*$suryo;
 
+            $honbun .= $name .'';
+            $honbun .= $price .'円 x';
+            $honbun .= $suryo .'個 =';
+            $honbun .= $shokei . "円 \n";
+        }
 
-            //<<--3.データベースから切断-->>
-            $dbh = null;
+        //<<--3.データベースから切断-->>
+        $dbh = null;
 
+        $honbun .= "送料は無料です。\n";
+        $honbun .= "-------------------\n";
+        $honbun .= "\n";
+        $honbun .= "代金は以下の口座にお振込ください。\n";
+        $honbun .= "ろくまる銀行 やさい支店 普通口座 1234567\n";
+        $honbun .= "入金確認が取れ次第、梱包、発送させていただきます。\n";
+        $honbun .= "\n";
 
+        $honbun .= "□□□□□□□□□□□□□□□□□□\n";
+        $honbun .= "　～安心野菜のろくまる農園～\n";
+        $honbun .= "\n";
+        $honbun .= "〇〇県六丸郡六丸村123-4\n";
+        $honbun .= "電話 090-6060-xxxx\n";
+        $honbun .= "メール info@rokumarunouen.co.jp\n";
+        $honbun .= "□□□□□□□□□□□□□□□□□□\n";
+        // print '<br/>';
+        // print nl2br($honbun);
 
+        $title = 'ご注文ありがとうございます。';
+        $header = 'From: info@rokumarunouen.co.jp';
+        $honbun = html_entity_decode($honbun, ENT_QUOTES, 'UTF-8');
+        mb_language('Japanese');
+        mb_internal_encoding('UTF-8');
+        mb_send_mail($email, $title, $honbun, $header);
 
     } catch (Exception $e) {
         print 'ただいま障害により大変ご迷惑をお掛けしております。';
